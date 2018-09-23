7ed412ab88a6c7162d10a4e14fb0f91dace9bf97
Andrew Jaworski <apjawors@gmail.com>
2018-09-23 02:10:26 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,121 +1,165 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Category from './components/Category'
+import Rules from './components/Rules'
 import Game from './components/Game'
-import {nba, nfl, nhl} from './components/CategoryLogic'
-import Football from './components/images/football.svg'
-import Basketball from './components/images/basketball.svg'
-import Baseball from './components/images/baseball.svg'
-import Puck from './components/images/puck.svg'
-import './index.css'
+import {nfl, nba, mlb, nhl} from './components/CategoryLogic'
+
 
 class App extends React.Component {
   constructor(props){
     super(props)
     this.state      = {league: null,
                        icon: null,
+                       playText: "Pick a league!",
+                       playClass: "choose",
                        start: false,
+                       rules: false,
                        fullscreen: true,
-                       category: null
+                       countdown: false,
+                       category: false
                       }
     this.play       = this.play.bind(this)
+    this.rules      = this.rules.bind(this)
     this.menu       = this.menu.bind(this)
     this.select     = this.select.bind(this)
     this.fullscreen = this.fullscreen.bind(this)
+    this.readySetGo = this.readySetGo.bind(this)
+  }
+  rules(){
+     this.setState(prevState => ({
+      rules: !prevState.rules
+    }));
   }
   select(league){
     if (league === "NFL"){
       this.setState({
         league: nfl,
         category: league,
-        icon: Football
+        icon: 'src/components/images/football.svg'
       })
     } else if (league === "NBA"){
       this.setState({
         league: nba,
         category: league,
-        icon: Basketball
+        icon: 'src/components/images/basketball.svg'
       })
     } else if (league === "MLB"){
       this.setState({
-        league: nba,
+        league: mlb,
         category: league,
-        icon: Baseball
+        icon: 'src/components/images/baseball.svg'
       })
     } else if (league === "NHL"){
       this.setState({
         league: nhl,
         category: league,
-        icon: Puck
+        icon: 'src/components/images/puck.svg'
       })
     }
+    this.setState({
+      playText: "Play!",
+      playClass: "ready"
+    })
   }
   fullscreen(){
-      this.setState(prevState => ({
-        fullscreen: !prevState.fullscreen
-      }));
-      let ele = document.documentElement
-      if (this.state.fullscreen){
-        if (ele.requestFullscreen) {
-          ele.requestFullscreen();
-        } else if (ele.webkitRequestFullscreen) {
-          ele.webkitRequestFullscreen();
-        } else if (ele.mozRequestFullScreen) {
-          ele.mozRequestFullScreen();
-        } else if (ele.msRequestFullscreen) {
-          ele.msRequestFullscreen();
-        } else {
-          console.log('Fullscreen API is not supported.');
-        }
+    this.setState(prevState => ({
+      fullscreen: !prevState.fullscreen
+    }));
+    let ele = document.documentElement
+    if (this.state.fullscreen){
+      if (ele.requestFullscreen) {
+        ele.requestFullscreen();
+      } else if (ele.webkitRequestFullscreen) {
+        ele.webkitRequestFullscreen();
+      } else if (ele.mozRequestFullScreen) {
+        ele.mozRequestFullScreen();
+      } else if (ele.msRequestFullscreen) {
+        ele.msRequestFullscreen();
       } else {
-        if (document.exitFullscreen) {
-          document.exitFullscreen();
-        } else if (document.webkitExitFullscreen) {
-          document.webkitExitFullscreen();
-        } else if (document.mozCancelFullScreen) {
-          document.mozCancelFullScreen();
-        } else if (document.msExitFullscreen) {
-          document.msExitFullscreen();
-        } else {
-          console.log('Fullscreen API is not supported.');
-        }
+        console.log('Fullscreen API is not supported.');
       }
-      console.log(this.state.fullscreen)
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+      } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+      } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+      } else {
+        console.log('Fullscreen API is not supported.');
+      }
     }
-    play(){
+  }
+  readySetGo(){
+    this.setState({
+      playText: 3,
+      countdown: true,
+    })
+    this.timerID = setInterval(
+      () => this.play(),
+      1000
+    )
+  }
+  play(){
+    if (this.state.playText > 1) {
       this.setState({
-        start: true
+        playText: this.state.playText - 1
       })
     }
-    menu(){
-      this.setState({
-        start: false,
-        league: null,
-        icon: null,
-        category: null
-      })
+    else {
+      if (this.state.playText === 1){
+        this.setState({
+          playText: "Go!"
+        })
+      }
+      else {
+        clearInterval(this.timerID);
+        this.setState({
+          start: true,
+          playText: "Pick a league!",
+          playClass: "choose",
+          rules: false,
+          countdown: false
+        })
+      }
     }
-    render(){
-      let playText  = this.state.league === null ? "Pick a league!" : "Play!",
-          playClass = this.state.league === null ? "choose" : "ready",
-          fullscreenText = this.state.fullscreen ? "Fullscreen (recommended)" : "Exit fullscreen",
-          cat = this.state.category
-      return (
-        <div className="app-grid">
+  }
+  menu(){
+    this.setState({
+      league: null,
+      icon: null,
+      start: false,
+      category: null
+    })
+  }
+  render(){
+    let fullscreenText = this.state.fullscreen ? "Fullscreen" : "Exit fullscreen",
+        cat = this.state.category, countdown = this.state.countdown
+    return (
+      <div className="app-grid">
         {!this.state.start ?
           <React.Fragment>
-            <div className="title-container">
-              <div className="title whose">WHOSE</div>
-              <div className="title hue">HUE?</div>
+            <div className="title-cont">
+              <div className="title shades">SHADES</div>
+              <div className="title of">of</div>
+              <div className="title sport">SPORTS</div>
             </div>
-            <Category class={"NFL"} category={cat} click={() => this.select("NFL")} />
-            <Category class={"NBA"} category={cat} click={() => this.select("NBA")} />
-            <Category class={"MLB"} category={cat} click={() => this.select("MLB")} />
-            <Category class={"NHL"} category={cat} click={() => this.select("NHL")} />
-            <div className={"menu-button play " + playClass} onClick={cat !== null ? this.play : null}>{playText}</div>
-            <div className="menu-button rules">Rules</div>
-            <div className="menu-button fs" onClick={this.fullscreen}>{fullscreenText}</div>
-            <div className="copyright">WhoseHue? &copy; 2018</div>
+            <Category class={"NFL"} category={cat} click={!countdown ? () => this.select("NFL") : null} />
+            <Category class={"NBA"} category={cat} click={!countdown ? () => this.select("NBA") : null} />
+            <Category class={"MLB"} category={cat} click={!countdown ? () => this.select("MLB") : null} />
+            <Category class={"NHL"} category={cat} click={!countdown ? () => this.select("NHL") : null} />
+            <div className={"button play " + this.state.playClass} onClick={cat && !countdown ? this.readySetGo : null}>{this.state.playText}</div>
+            <div className="button rules" onClick={this.rules}>Rules</div>
+            <div className="button fs" onClick={this.fullscreen}>{fullscreenText}</div>
+            {this.state.rules ?
+              <Rules click={this.rules} />  
+              :
+              null
+            }
+            <div className="creator">By <span style={{color: "#4FB286"}}>Andrew J</span></div>
           </React.Fragment>
             :
             <Game league={this.state.league} icon={this.state.icon} menu={this.menu}/>
