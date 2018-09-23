3f8cd71c754b557be17d65f008cbc0cc8ac62413
Andrew Jaworski <apjawors@gmail.com>
2018-09-23 02:03:37 -0400

    Update Team.js

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import './styles/Team.css'
 
 const Team = props => {
   return (
-    <div className={"team t" + props.class} onClick={props.click}>{props.team}</div>
+    <div className={"team t" + props.class} onClick={props.click}>{props.team}</div> 
   )
 }
 
